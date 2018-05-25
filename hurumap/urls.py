3fee2fd168bfc8fc19e16a1dfeb5d13033bb9d19
Phillip Ahereza <pahereza@gmail.com>
2018-05-25 16:11:01 +0300

    add static root to urls

--- hurumap/urls.py
+++ hurumap/urls.py
@@ -5,4 +5,6 @@ from wazimap.urls import urlpatterns as wazimap_urlpatterns
 from hurumap import settings
 from hurumap.admin.urls import urlpatterns as hurumap_admin_urlpatterns
 
-urlpatterns = hurumap_admin_urlpatterns + wazimap_urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = hurumap_admin_urlpatterns + wazimap_urlpatterns + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
