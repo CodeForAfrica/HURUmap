017a1795a2242885ba4af1c61ce29f8b7d323b5b
Phillip Ahereza <pahereza@gmail.com>
2018-06-13 09:08:55 +0300

    add indicators with atleast one of the 10 countries

--- hurumap/management/commands/scrapewb.py
+++ hurumap/management/commands/scrapewb.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 
 import wbdata
-from hurumap.models.data import DataIndicatorAuthor, DataIndicator, DataIndicatorValue
+from hurumap.models.data import DataIndicatorAuthor, DataIndicator, \
+    DataIndicatorValue
 
 
 class Command(BaseCommand):
@@ -17,40 +18,62 @@ class Command(BaseCommand):
             }
         )
 
-        wb_indicators = wbdata.get_indicator()
+        wb_indicators = wbdata.get_indicator(display=False)
         for wb_indicator in wb_indicators:
             if wb_indicator.get('id', 'lema') == 'lema':
                 raise CommandError(str(wb_indicator))
-            
-            indicator, created = DataIndicator.objects.get_or_create(
-                author=author,
-                author_code=wb_indicator.get('id'),
-                defaults={
-                    'author_data': wb_indicator,
-                    'name': wb_indicator.get('name'),
-                    'source': wb_indicator.get('source'),
-                    'source_note': wb_indicator.get('sourceNote'),
-                    'topics': list(wb_indicator.get('topics'))
-                }
-            )
 
-            # takwimu_countries = 
-            wb_indicator_values = wbdata.get_data(
-                wb_indicator.get('id'),
-                country=["KE","NG","SN","TZ","BF","ZM","ZA","UG","ET","CD"]
-            )
-            for wb_indicator_val in wb_indicator_values:
-                indicator_val, created = DataIndicatorValue.objects.get_or_create(
-                    indicator=indicator,
-                    country=wb_indicator_val.get('country'),
-                    date=wb_indicator_val.get('date'),
+            try:
+                # takwimu_countries =
+                wb_indicator_values = wbdata.get_data(
+                    wb_indicator.get('id'),
+                    country=["KE", "NG", "SN", "TZ", "BF", "ZM", "ZA", "UG",
+                             "ET", "CD"]
+                )
+
+                if len(filter(self.filter_indicators,
+                              wb_indicator_values)) == 0:
+                    self.stdout.write(self.style.WARNING(
+                        'Skipped indicator value "%s"' % wb_indicator.get(
+                            'id')))
+                    continue
+
+                indicator, created = DataIndicator.objects.get_or_create(
+                    author=author,
+                    author_code=wb_indicator.get('id'),
                     defaults={
-                        'author_data': wb_indicator_val,
-                        'decimal': wb_indicator_val.get('decimal'),
-                        'value': wb_indicator_val.get('value')
+                        'author_data': wb_indicator,
+                        'name': wb_indicator.get('name'),
+                        'source': wb_indicator.get('source'),
+                        'source_note': wb_indicator.get('sourceNote'),
+                        'topics': list(wb_indicator.get('topics'))
                     }
                 )
-                self.stdout.write(self.style.SUCCESS('Successfully created indicator value"%s"' % wb_indicator.get('id')))
 
-            self.stdout.write(self.style.SUCCESS('Successfully created indicator "%s"' % wb_indicator.get('id')))
-            
+                for wb_indicator_val in wb_indicator_values:
+                    indicator_val, created = DataIndicatorValue.objects.get_or_create(
+                        indicator=indicator,
+                        country=wb_indicator_val.get('country'),
+                        date=wb_indicator_val.get('date'),
+                        defaults={
+                            'author_data': wb_indicator_val,
+                            'decimal': wb_indicator_val.get('decimal'),
+                            'value': wb_indicator_val.get('value')
+                        }
+                    )
+                    self.stdout.write(self.style.SUCCESS(
+                        'Successfully created indicator value "%s"' % wb_indicator.get(
+                            'id')))
+            except Exception as e:
+                self.stdout.write(self.style.NOTICE(
+                    'NOTICE "%s"' % e.message))
+                continue
+
+            self.stdout.write(self.style.SUCCESS(
+                'Successfully created indicator "%s"' % wb_indicator.get('id')))
+
+    def filter_indicators(self, indicator):
+        if indicator.get('value', None):
+            return True
+        else:
+            return False
