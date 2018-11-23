1cc7acd8b029030e0117db3cfcb24968ec24fdee
Phillip Ahereza <pahereza@gmail.com>
2018-11-23 11:10:50 +0300

    rename tables file

--- hurumap/tables/afrobarometer.py
+++ hurumap/tables/afrobarometer.py
@@ -1,28 +1,31 @@
 from wazimap.data.tables import FieldTable, SimpleTable
 
-FieldTable(['access_to_information'], universe='Land in South Africa',
-           year='2017', dataset='Afrobarometer Survey')
 
-FieldTable(['allow_farmers_retain_land_ownership'],
-           universe='Land in South Africa', year='2017',
-           dataset='Afrobarometer Survey')
+# TODO move to HURUmap_land app tables since the the land data is unique to only hurumap_land
 
-FieldTable(['maintain_willing_buyer_willing_seller_policy'],
-           universe='Land in South Africa', year='2017',
-           dataset='Afrobarometer Survey')
-
-FieldTable(['land_acquisation_challenges'], universe='Land in South Africa',
-           year='2017', dataset='Afrobarometer Survey')
-
-FieldTable(['land_to_prioritise_for_redistribution'],
-           universe='Land in South Africa', year='2017',
-           dataset='Afrobarometer Survey')
-
-FieldTable(['women_have_equal_right_to_land'], universe='Land in South Africa',
-           year='2017', dataset='Afrobarometer Survey')
-
-FieldTable(['women_men_equal_chance_own_land'], universe='Land in South Africa',
-           year='2017', dataset='Afrobarometer Survey')
+# FieldTable(['access_to_information'], universe='Land in South Africa',
+#            year='2017', dataset='Afrobarometer Survey')
+#
+# FieldTable(['allow_farmers_retain_land_ownership'],
+#            universe='Land in South Africa', year='2017',
+#            dataset='Afrobarometer Survey')
+#
+# FieldTable(['maintain_willing_buyer_willing_seller_policy'],
+#            universe='Land in South Africa', year='2017',
+#            dataset='Afrobarometer Survey')
+#
+# FieldTable(['land_acquisation_challenges'], universe='Land in South Africa',
+#            year='2017', dataset='Afrobarometer Survey')
+#
+# FieldTable(['land_to_prioritise_for_redistribution'],
+#            universe='Land in South Africa', year='2017',
+#            dataset='Afrobarometer Survey')
+#
+# FieldTable(['women_have_equal_right_to_land'], universe='Land in South Africa',
+#            year='2017', dataset='Afrobarometer Survey')
+#
+# FieldTable(['women_men_equal_chance_own_land'], universe='Land in South Africa',
+#            year='2017', dataset='Afrobarometer Survey')
 
 FieldTable(['own_radio'], year=2016, dataset='Afrobarometer Survey',
            id='own_radio')
