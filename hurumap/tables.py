49bd1abd4bf3a6cfd32ff236e5f0a6c415b42948
Phillip Ahereza <pahereza@gmail.com>
2018-11-23 07:33:51 +0300

    updated fixtures

--- hurumap/tables.py
+++ hurumap/tables.py
@@ -6,84 +6,107 @@ from wazimap.data.tables import FieldTable
 
 FieldTable(
     ['rural or urban', 'sex', 'age in completed years'],
-    year='2009'
+    year='2009', dataset='Census'
 )
 FieldTable(
     ['employment activity status', 'sex'],
     universe='People aged 5 years and older',
-    year='2009'
+    year='2009', dataset='Census'
 )
 FieldTable(
     ['school attendance', 'sex'],
     universe='People aged 3 years and older',
-    year='2009'
+    year='2009', dataset='Census'
 )
 FieldTable(
     ['highest education level reached'],
     universe='People aged 3 years and older',
-    year='2009'
+    year='2009', dataset='Census'
 )
 FieldTable(
     ['main mode of human waste disposal'],
     universe='Households',
-    year='2009'
+    year='2009', dataset='Census'
 )
 FieldTable(
     id='religion',
     fields=['religion'],
-    year=2009
+    year=2009, dataset='Census'
 )
 
 FieldTable(
     id='household_heads',
     fields=['Household_Heads'],
-    year=2009
+    year=2009, dataset='Census'
 )
-FieldTable(['main source of water'], universe='Households', year='2009')
-FieldTable(['main type of lighting fuel'], universe='Households', year='2009')
-FieldTable(['main type of floor material'], universe='Households', year='2009')
-FieldTable(['main type of wall material'], universe='Households', year='2009')
+FieldTable(['main source of water'], universe='Households', year='2009',
+           dataset='Census')
+FieldTable(['main type of lighting fuel'], universe='Households', year='2009',
+           dataset='Census')
+FieldTable(['main type of floor material'], universe='Households', year='2009',
+           dataset='Census')
+FieldTable(['main type of wall material'], universe='Households', year='2009',
+           dataset='Census')
 FieldTable(
     ['main type of roofing material'],
     universe='Households',
-    year='2009'
+    year='2009', dataset='Census'
 )
 
-
-FieldTable(['contraceptive_use'], universe='Contraceptive_use', year='2014')
+FieldTable(['contraceptive_use'], universe='Contraceptive_use', year='2014', dataset='KNBS Demographic and Health Survey')
 FieldTable(
     ['contraceptive_modern_method'],
     universe='Contraceptive_use',
-    year='2014'
+    year='2014', dataset='KNBS Demographic and Health Survey'
 )
 FieldTable(
     ['contraceptive_traditional_method'],
     universe='Contraceptive_use',
-    year='2014'
+    year='2014', dataset='KNBS Demographic and Health Survey'
 )
 FieldTable(
     ['maternal care indicators'],
     universe='Maternal care indicators',
-    year='2014'
+    year='2014', dataset='KNBS Demographic and Health Survey'
 )
-FieldTable(['use of ipt'], universe='Maternal care indicators', year='2014')
-FieldTable(['knowledge of hiv prevention methods', 'sex'], year='2014')
-FieldTable(['household possession of itn'], universe='ITN', year='2014')
-FieldTable(['household', 'users', 'itn_use'], universe='ITN', year='2014')
-FieldTable(['fertility'], year='2014')
-FieldTable(['vaccinations'], year='2014')
-FieldTable(['weight for age'], universe='Nutrition', year='2014')
-FieldTable(['height for age'], universe='Nutrition', year='2014')
-FieldTable(['weight for height'], universe='Nutrition', year='2014')
-FieldTable(['type', 'treatment'], year='2014')
-FieldTable(['treatment of children with fever'], year='2014')
-FieldTable(['protests'], year='2014')
-FieldTable(['schoolfires'], year='2016')
-FieldTable(['crimereport'], year='2015')
-FieldTable(['typesofcrime'], year='2015')
-FieldTable(['healthratios'], year='2014')
-FieldTable(['voterregistration_2015'], year='2014')
-FieldTable(['voterregistration_2017'], year='2017')
-FieldTable(['crop_production'], year='2014')
-FieldTable(['livestock_population'], year='2014')
-FieldTable(['livestock_products'], year='2014')
+FieldTable(['use of ipt'], universe='Maternal care indicators', year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['knowledge of hiv prevention methods', 'sex'], year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['household possession of itn'], universe='ITN', year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['household', 'users', 'itn_use'], universe='ITN', year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['fertility'], year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['vaccinations'], year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['weight for age'], universe='Nutrition', year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['height for age'], universe='Nutrition', year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['weight for height'], universe='Nutrition', year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['type', 'treatment'], year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['treatment of children with fever'], year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['protests'], year='2014', dataset='Annual Crimes Report')
+FieldTable(['schoolfires'], year='2016', dataset='Annual Crimes Report')
+FieldTable(['crimereport'], year='2015', dataset='Annual Crimes Report')
+FieldTable(['typesofcrime'], year='2015', dataset='Annual Crimes Report')
+FieldTable(['healthratios'], year='2014',
+           dataset='KNBS Demographic and Health Survey')
+FieldTable(['voterregistration_2015'], year='2014',
+           dataset='Voter Registration')
+FieldTable(['voterregistration_2017'], year='2017',
+           dataset='Voter Registration')
+FieldTable(['crop_production'], year='2014',
+           dataset='Crop production and Livestock',
+           description='Crop production and Livestock population for the year 2014 provided by the Ministry of Agriculture, Livestock and Fisheries.')
+FieldTable(['livestock_population'], year='2014',
+           dataset='Crop production and Livestock',
+           description='Crop production and Livestock population for the year 2014 provided by the Ministry of Agriculture, Livestock and Fisheries.')
+FieldTable(['livestock_products'], year='2014',
+           dataset='Crop production and Livestock',
+           description='Crop production and Livestock population for the year 2014 provided by the Ministry of Agriculture, Livestock and Fisheries.')
