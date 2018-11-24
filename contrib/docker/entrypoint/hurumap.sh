0d5a23d7697ab617ec91376536819051014a1444
David Lemayian <dlemayian@gmail.com>
2018-11-24 15:13:58 +0300

    Update hurumap.sh

--- contrib/docker/entrypoint/hurumap.sh
+++ contrib/docker/entrypoint/hurumap.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+until psql $DATABASE_URL -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing"
 
 python manage.py migrate --noinput                # Apply database migrations
 python manage.py compilescss                      # Compile SCSS (offline)
