af7e06d8bed0749ec52110eb9589935a68ecb5c5
Phillip Ahereza <pahereza@gmail.com>
2019-01-23 06:48:07 +0300

    raw data from tables

---
+++ hurumap/utils.py
@@ -0,0 +1,36 @@
+from wazimap.models import DBTable
+from wazimap.data.base import Base
+from wazimap.data.utils import get_session, get_datatable
+
+
+def get_table_data(table_name, session, geo_code, geo_level):
+    table = get_datatable(table_name)
+    if table:
+        description = table.description
+        data = session.query(Base.metadata.tables[table_name]).filter(Base.metadata.tables[table_name].c.geo_level == geo_level).filter(Base.metadata.tables[table_name].c.geo_code == geo_code).all()
+
+        return {
+            'description': description,
+            'raw_data': data,
+            'table_name': table_name
+        }
+
+
+
+def raw_data_for_geography(geo_code, geo_level):
+    Base.metadata.reflect()
+    session = get_session()
+    tables = DBTable.objects.all()
+
+    final_data = []
+    for table in tables:
+        data = get_table_data(table.name, session, geo_code, geo_level)
+        final_data.append(data)
+
+    return final_data
+
+
+
+
+
+
--- hurumap/views.py
+++ hurumap/views.py
@@ -1,46 +1,7 @@
-import json
-
-from django.conf import settings
-from django.core.serializers.json import DjangoJSONEncoder
-from django.http import Http404
-from django.utils.module_loading import import_string
-from django.utils.safestring import SafeString
-from wazimap.data.utils import dataset_context, get_page_releases
-from wazimap.geo import geo_data
-from wazimap.profiles import enhance_api_data
 from wazimap.views import GeographyDetailView
 
+from .utils import raw_data_for_geography
 
-def _get_values(obj):
-    try:
-        return obj['values']['this']
-
-    except TypeError:
-        pass
-
-
-def _get_description_of_data_point(obj):
-    # check if it has name value
-    if obj.has_key('name'):
-        return obj['name']
-    elif obj.has_key('metadata'):
-        return obj['metadata']
-
-    # check if it has metadata and metadata has name
-
-    # else get the key and replace _ with space and capitalize
-    pass
-
-
-def get_data(d):
-    for k, v in d.iteritems():
-        if isinstance(v, dict) and not v.has_key('values') and k != 'metadata':
-            get_data(v)
-        elif isinstance(v, dict) and v.has_key('values'):
-            print "{0} : {1}\n".format(k, _get_values(v))
-            yield k
-        else:
-            pass
 
 
 class PDFView(GeographyDetailView):
@@ -51,31 +12,9 @@ class PDFView(GeographyDetailView):
     def get_context_data(self, *args, **kwargs):
         page_context = {}
 
-        # load the profile
-        profile_method = settings.WAZIMAP.get('profile_builder', None)
-        self.profile_name = settings.WAZIMAP.get('default_profile', 'default')
-
-        if not profile_method:
-            raise ValueError(
-                "You must define WAZIMAP.profile_builder in settings.py")
-        profile_method = import_string(profile_method)
-
-        year = self.request.GET.get('release',
-                                    geo_data.primary_release_year(self.geo))
-        if settings.WAZIMAP['latest_release_year'] == year:
-            year = 'latest'
-
-        with dataset_context(year=year):
-            profile_data = profile_method(self.geo, self.profile_name,
-                                          self.request)
-
-        profile_data['geography'] = self.geo.as_dict_deep()
-        profile_data['primary_releases'] = get_page_releases(
-            settings.WAZIMAP['primary_dataset_name'], self.geo, year)
-
-        profile_data = enhance_api_data(profile_data)
-        page_context.update(profile_data)
-
+        raw_data = raw_data_for_geography(self.geo.geo_code, self.geo.geo_level)
+        page_context['raw_data'] = raw_data
+        page_context['geo'] = self.geo
         return page_context
 
     def get_template_names(self):
