9d6e37a4e362a24f8a48ca64ba916d7aff5eb57a
Phillip Ahereza <pahereza@gmail.com>
2019-01-23 13:06:48 +0300

    changes

--- hurumap/urls.py
+++ hurumap/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls.static import static
 from wazimap.urls import urlpatterns as wazimap_urlpatterns
 
 from hurumap import settings
-from hurumap.views import PDFView
+from hurumap.views import PDFView, pdf_view
 
 urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
               wazimap_urlpatterns
 urlpatterns += [
     url(
         regex='^data/pdf/(?P<geography_id>\w+-\w+)(-(?P<slug>[\w-]+))?/$',
-        view=PDFView.as_view(),
+        view=pdf_view,
         name='data-pdf',)
 ]
--- hurumap/views.py
+++ hurumap/views.py
@@ -1,4 +1,9 @@
+from django.conf import settings
+from django.http import Http404, HttpResponse
+from django.template.loader import render_to_string
+from wazimap.geo import geo_data, LocationNotFound
 from wazimap.views import GeographyDetailView
+from weasyprint import HTML, CSS
 
 from .utils import raw_data_for_geography
 
@@ -18,4 +23,29 @@ class PDFView(GeographyDetailView):
 
     def get_template_names(self):
         return ['pdf_data_template.html']
+
+
+def pdf_view(request, **kwargs):
+    version = request.GET.get('geo_version', None)
+    geo_id = kwargs.get('geography_id', None)
+
+    try:
+        geo_level, geo_code = geo_id.split('-', 1)
+        geo = geo_data.get_geography(geo_code, geo_level,
+                                          version)
+        page_context = {'geo': geo}
+        raw_data = raw_data_for_geography(geo_code, geo_level)
+        page_context['raw_data'] = raw_data
+        page_context['geo'] = geo
+
+        html_template = render_to_string(
+            'pdf_data_template.html', context=page_context)
+        pdf_file = HTML(string=html_template,
+                        base_url=request.build_absolute_uri()).write_pdf()
+        response = HttpResponse(pdf_file, content_type='application/pdf')
+        response['Content-Disposition'] = 'filename="hurumap-data.pdf"'
+        return response
+    except (ValueError, LocationNotFound):
+        raise Http404
+
 
