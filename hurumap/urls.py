12f86aa3ff0fb066e13499247e7225f9d0035474
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-07-24 09:58:04 +0300

    add geo comparison endpoint

--- hurumap/urls.py
+++ hurumap/urls.py
@@ -3,6 +3,16 @@ from django.conf.urls.static import static
 from wazimap.urls import urlpatterns as wazimap_urlpatterns
 
 from hurumap import settings
+from .views import GeographyCompareAPIView
 
-urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-              wazimap_urlpatterns
+STANDARD_CACHE_TIME = settings.HURUMAP['cache_secs']
+
+urlpatterns = [
+                url(
+                    regex   = '^api/compare/(?P<geo_id1>\w+-\w+)/vs/(?P<geo_id2>\w+-\w+)/$',
+                    view    = cache_page(STANDARD_CACHE_TIME)(GeographyCompareAPIView.as_view()),
+                    kwargs  = {},
+                    name    = 'api_geography_compare',
+                ),] + \
+                static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+                wazimap_urlpatterns
